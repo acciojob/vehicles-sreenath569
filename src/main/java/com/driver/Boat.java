c4342c2bbf427fa0488efefe6ba2eb5b2fd640e0
sreenath <sreenathkommuru@gmail.com>
2023-04-16 14:50:28 +0530

    second commit

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -5,6 +5,11 @@ public class Boat implements WaterVehicle{
     private String name;
     private int capacity;
 
+    public Boat(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
     public String getVehicleName() {
         return name;
     }
